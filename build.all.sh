22c93144d0ae8a6701c60314df832a7d6912b44d
Synapticloop <synapticloop@users.noreply.github.com>
2023-10-27 21:28:02 +1100

    complete regen including sqlite3

--- build.all.sh
+++ build.all.sh
@@ -5,5 +5,6 @@
 ./gradlew --no-daemon -b build.h2zero.cockroach.gradle h2zero
 ./gradlew --no-daemon -b build.h2zero.mysql.gradle h2zero
 ./gradlew --no-daemon -b build.h2zero.postgresql.gradle h2zero
+./gradlew --no-daemon -b build.h2zero.sqlite3.gradle h2zero
 
 
