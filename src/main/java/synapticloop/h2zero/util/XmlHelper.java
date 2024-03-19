ca7a8c62bdaa3b065a867e2cfce0285059903191
synapticloop <contact@synapticloop.com>
2024-03-19 11:14:26 +1100

    Add in getOrSet static method

---
+++ src/main/java/synapticloop/h2zero/util/XmlHelper.java
@@ -0,0 +1,11 @@
+package synapticloop.h2zero.util;
+
+public class XmlHelper {
+	public static String escapeXml(String s) {
+		return s.replaceAll("&", "&amp;")
+				.replaceAll(">", "&gt;")
+				.replaceAll("<", "&lt;")
+				.replaceAll("\"", "&quot;")
+				.replaceAll("'", "&apos;");
+	}
+}
